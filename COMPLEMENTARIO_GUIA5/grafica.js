55887f646502b3c4d2ea2f4f16dc6f3d737a17e5
Beltranpat7 <141784863+Beltranpat7@users.noreply.github.com>
2023-09-15 08:56:51 -0600

    Add files via upload

---
+++ COMPLEMENTARIO_GUIA5/grafica.js
@@ -0,0 +1,46 @@
+function graficar() {
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+    const color = document.getElementById('colorPicker').value;
+    const orientacion = document.querySelector('input[name="orientacion"]:checked').value;
+
+    const datosNavegadores = { 'Chrome': 70, 'Firefox': 20, 'Safari': 10 };
+    const datosSistemas = { 'Windows': 60, 'MacOS': 30, 'Linux': 10 };
+
+    const datos = tipo === 'navegadores' ? datosNavegadores : datosSistemas;
+
+    const grafico = document.getElementById('grafico');
+    grafico.innerHTML = ''; 
+
+    let positionOffset = 0;
+
+    for (const key in datos) {
+        // Crear la barra
+        const barra = document.createElement('div');
+        barra.className = 'barra';
+        barra.style.backgroundColor = color;
+        if (orientacion === 'vertical') {
+            barra.style.height = `${datos[key]}%`;
+            barra.style.left = `${positionOffset}px`;
+        } else {
+            barra.className += ' horizontal';
+            barra.style.width = `${datos[key]}%`;
+            barra.style.top = `${positionOffset}px`;
+        }
+        grafico.appendChild(barra);
+
+        
+        const etiqueta = document.createElement('div');
+        etiqueta.textContent = `${key}: ${datos[key]}%`;
+        etiqueta.className = 'etiqueta';
+        if (orientacion === 'horizontal') {
+            etiqueta.className += ' horizontal';
+            etiqueta.style.top = `${positionOffset + 15}px`;
+        } else {
+            etiqueta.style.left = `${positionOffset}px`;
+        }
+        grafico.appendChild(etiqueta);
+
+        positionOffset += 60; 
+    }
+}
+
